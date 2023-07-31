74a92ff718c97afcf02edf48b9852d76e24e3797
Gaguchi <gaga9393@gmail.com>
2023-07-31 19:08:02 +0400

    auto-active

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ let currentData = [];
 const audio = new Audio('sound-file.mp3'); // Replace 'notification_sound.mp3' with the actual path to your audio file
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    
+  console.log(message);
 
   if (message.data) {
     // Compare the new data with the previous data
@@ -26,3 +26,29 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
   }
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    var runningTabId;
+    chrome.storage.local.get("runningTabId", function (result) {
+      runningTabId =result.runningTabId ;
+      // Check if the URL of the updated tab contains the desired part
+    if (tab.url.includes("app.centraldispatch.com/search") && tabId === runningTabId) {
+      // Send the message to the content script of the new tab
+
+      chrome.tabs.executeScript(tabId, { file: 'content_script.js' })
+      switchToTab(tabId);
+      chrome.tabs.sendMessage(tabId, "start");
+    }
+    });
+
+    
+  }
+});
+
+function switchToTab(tabId) {
+  chrome.tabs.update(tabId, { active: true }, (updatedTab) => {
+    // The active tab has been switched to the specified tabId.
+    console.log(`Switched to tab with ID: ${tabId}`);
+  });
+}
